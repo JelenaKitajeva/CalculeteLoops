7c49aaa106c62afb8ffe229bbef4f0e88e62b818
Jelena Kitajeva <lena_terehhova@mail.ru>
2022-04-06 16:09:10 +0300

    Loops

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"{i}Vampire is not career choice!");
+            }
+            Console.WriteLine("Have a nice day!");
+
+        }
+    }
+}
